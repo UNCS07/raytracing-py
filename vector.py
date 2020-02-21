079462973769fcfd63dee68a636895edf2ffe8e2
UNCS07 <kutuzov.stanislav@mail.ru>
2020-02-21 12:55:30 +0300

    Getting started. Added Vector class with basic functionality of vectors.

---
+++ vector.py
@@ -0,0 +1,57 @@
+from math import sqrt
+
+class Vector:
+
+    def __init__(self, o = (0,0,0), d = (0,0,0)):
+        self.o = o
+        self.d = d
+
+    def __add__(self, other):
+        if type(other) is not Vector:
+            raise TypeError
+        o = self.o
+        d = tuple([other.d[i]+self.d[i]-other.o[i]-self.o[i] for i in range(3)])
+        result = Vector(o, d)
+        return result
+
+    def __sub__(self, other):
+        if type(other) is not Vector:
+            raise TypeError
+        o = self.o
+        d = tuple([other.d[i]-self.d[i]-other.o[i]-self.o[i] for i in range(3)])
+        result = Vector(o, d)
+        return result
+
+    def __mul__(self, other):
+        if type(other) in (Vector, str):
+            raise TypeError
+        o = self.o
+        d = tuple([(self.d[i]-self.o[i])*other+self.o[i] for i in range(3)])
+        result = Vector(o, d)
+        return result
+
+    def __truediv__(self, other):
+        if type(other) in (Vector, str):
+            raise TypeError
+        o = self.o
+        d = tuple([(self.d[i] - self.o[i]) / other + self.o[i] for i in range(3)])
+        result = Vector(o, d)
+        return result
+
+    def __str__(self):
+        return str(self.o)+str(self.d)
+
+    def magn(self):
+        sum = 0
+        for i in range(3):
+            sum += pow(self.d[i] - self.o[i], 2)
+        return sqrt(sum)
+
+    def norm(self):
+        return self/self.magn()
+
+    def dot(self, other):
+        sum = 0
+        for i in range(3):
+            sum += (self.d[i] - self.o[i])*(other.d[i] - other.o[i])
+        return sum
